f2789cd7cde723e8178d0065329081df254bf7da
Melanie E Magdalena <info@melaniemagdalena.com>
2018-02-10 18:06:26 -0700

    example

---
+++ testimonial.js
@@ -0,0 +1,17 @@
+var slideIndex = 0;
+showLis(slideIndex);
+
+function plusLis(n) {
+    showLis(slideIndex += n);
+}
+
+function showLis(n) {
+    var i;
+    var x = document.getElementsByClassName("testimonial");
+    if (n > x.length) {slideIndex = 0}
+    if (n < 0) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-0].style.display = "flex";
+}
